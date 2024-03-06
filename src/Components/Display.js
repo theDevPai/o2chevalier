3d34bc17d4109afe84746413d652f7e805b0a5d3
Nandan Pai <100035968+theDevPai@users.noreply.github.com>
2024-03-06 14:59:51 +0530

    Update Display.js

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import Table from "react-bootstrap/Table";
-import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
